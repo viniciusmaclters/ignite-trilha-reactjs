c26de29d44a0712c326c54cbf682d3bf7bae2d22
viniciusmaclters <vinicius.maclters@hotmail.com>
2022-12-29 09:32:20 -0300

    Iniciando trila reactjs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </>
+  )
+}
+
