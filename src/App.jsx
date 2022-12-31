e801801d257c38f57b8f1f275772fc5ebadcb22a
viniciusmaclters <vinicius.maclters@hotmail.com>
2022-12-31 10:44:51 -0300

    global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { Post } from "./Post"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import './global.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post 
         author="Vinicius Maclters" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio blanditiis quaerat in nisi. Totam, fuga molestiae labore iste tenetur nemo repellendus harum sequi sapiente, commodi vel quam deserunt, culpa iusto!"
