6910db998331da8ae1a1ca3feb293c4624f7e7e0
viniciusmaclters <vinicius.maclters@hotmail.com>
2022-12-31 08:46:59 -0300

    Estrutura da aplicacao

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { Post } from "./Post"
 export function App() {
   return (
     <>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+        author="Vinicius Maclters" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio blanditiis quaerat in nisi. Totam, fuga molestiae labore iste tenetur nemo repellendus harum sequi sapiente, commodi vel quam deserunt, culpa iusto!"
+      />
     </>
   )
 }
-
