70bafe4da7a2ce08f0326487dc97152d9b4d5252
viniciusmaclters <vinicius.maclters@hotmail.com>
2023-01-01 10:02:48 -0300

    Iniciado Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
+
 import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author="Vinicius Maclters" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio blanditiis quaerat in nisi. Totam, fuga molestiae labore iste tenetur nemo repellendus harum sequi sapiente, commodi vel quam deserunt, culpa iusto!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
+         author="Vinicius Maclters" 
+         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio blanditiis quaerat in nisi. Totam, fuga molestiae labore iste tenetur nemo repellendus harum sequi sapiente, commodi vel quam deserunt, culpa iusto!"
+        />
+
+        <Post 
+         author="Matheus Maclters" 
+         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio blanditiis quaerat in nisi. Totam, fuga molestiae labore iste tenetur nemo repellendus harum sequi sapiente, commodi vel quam deserunt, culpa iusto!"
+        />
+        </main>
+      </div>
     </>
   )
 }
