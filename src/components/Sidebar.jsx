5fdf68ea041095c731034e42adb1b0abbf38af5f
viniciusmaclters <vinicius.maclters@hotmail.com>
2023-01-01 16:08:54 -0300

    Finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,14 @@ export function Sidebar() {
       />
      
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/48839930?v=4" />
         <strong>Vinicius Maclters</strong>
         <span>Web Developer</span>
       </div>
      
       <footer>
         <a href="#">
+          <PencilSimpleLine size={20} />
           Editar seu perfil
         </a>
       </footer>
