7f13c1af4ed3cdad600f9d509f0cb54f5a749a9e
viniciusmaclters <vinicius.maclters@hotmail.com>
2023-01-03 09:30:02 -0300

    Estado(useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2])
+  
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBr
   })
@@ -13,6 +16,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBr,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -40,7 +49,7 @@ export function Post({ author, publishedAt, content }) {
        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixei seu feedback</strong>
 
         <textarea
@@ -53,9 +62,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-      <Comment />
-      <Comment />
-      <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
     )
