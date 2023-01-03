531bc71f2f26a058578d8b3a7ffc772ee8aabb88
viniciusmaclters <vinicius.maclters@hotmail.com>
2023-01-03 08:49:44 -0300

    Utilizados metodos date e map

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const posts = [
       {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type: 'link', content: 'jane.design/doctorcare'},
     ],
-    publishedAt: new Date('2022-01-01 10:00:00')
+    publishedAt: new Date('2023-01-01 10:00:00')
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const posts = [
       {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type: 'link', content: 'jane.design/doctorcare'},
     ],
-    publishedAt: new Date('2022-01-02 11:00:00')
+    publishedAt: new Date('2023-01-02 11:00:00')
   },
 ]
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,19 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
+
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post({ author }) {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBr
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBr,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
@@ -13,21 +25,19 @@ export function Post({ author }) {
           </div>
         </div>
 
-        <time title='02 de Janeiro às 06:23h' dateTime='2023-01-02 06:23:30'>Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-       <p>Fala galeraa 👋</p>
-
-       <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-       <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
-       <p>
-          <a href="">#novoprojeto</a>{' '} {/* serve para dar um espaço. Não recomendado */}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>{' '}
-       </p>
+       {content.map(line => {
+        if (line.type === 'paragraph') {
+          return <p>{line.content}</p>
+        } else if (line.type === 'link') {
+          return <p><a href="#">{line.content}</a></p>
+        }
+       })}
       </div>
 
       <form className={styles.commentForm}>
