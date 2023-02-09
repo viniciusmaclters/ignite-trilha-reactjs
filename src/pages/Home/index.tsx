0972240282db79fd6117c60a6a16152d00a291c7
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-02-09 19:45:02 -0300

    Contexto entre rotas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,31 +2,13 @@ import { HandPalm, Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
 import * as zod from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { createContext, useState } from "react"
+import { useContext } from "react"
  
 import { HomeContainer, StartCountdownButton, StopCountdownButton } from "./styles"
 import { NewCycleForm } from "./components/NewCycleForm"
 import { CountDown } from "./components/Countdown"
 import { FormProvider } from "react-hook-form"
-
-interface Cycle {
-  id: string,
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
-}
-
-interface CyclesContextType {
-  activeCycle: Cycle | undefined
-  activeCycleId: string | null
-  amountSecondsPassed: number
-  markCurrentCycleAsFinished: () => void
-  setSecondsPassed: (seconds: number) => void
-}
-
-export const CyclesContext = createContext({} as CyclesContextType)
+import { CyclesContext } from "../../contexts/CyclesContext"
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -36,9 +18,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const { activeCycle, createNewCicle, interruptCurrentCycle } = useContext(CyclesContext)
 
   const newCycleForm = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -50,76 +30,19 @@ export function Home() {
 
   const { handleSubmit, watch, reset } = newCycleForm
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-
-  function setSecondsPassed(seconds: number) {
-    setAmountSecondsPassed(seconds)
-  }
-
-  function markCurrentCycleAsFinished() {
-      setCycles((state) => state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-         return { ...cycle, finishedDate: new Date()}
-        } else {
-         return cycle
-        }
-      }), 
-    )
-  }
-
-  function handleCreateNewCicle(data: NewCycleFormData) {
-    const id = String(new Date().getTime())
-
-    const newCycle: Cycle = {
-      id,
-      task: data.task,
-      minutesAmount: data.minutesAmount,
-      startDate: new Date(),
-    }
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
-    setAmountSecondsPassed(0)
-
-    reset()
-  }
-
-  function handleInterruptCycle() {
-    setCycles((state) => state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date()}
-        } else {
-          return cycle
-        }
-      })
-    )
-    setActiveCycleId(null)
-  }
-
   const task = watch('task')
   const isSubmitDisabled = !task // variável de explicação para facilitar o entendimento do código
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
-
-        <CyclesContext.Provider 
-          value={{ 
-              activeCycle, 
-              activeCycleId, 
-              markCurrentCycleAsFinished, 
-              amountSecondsPassed,
-              setSecondsPassed,
-            }}
-          >
-
+      <form onSubmit={handleSubmit(createNewCicle)} action="">
           <FormProvider {...newCycleForm}>
             <NewCycleForm />
           </FormProvider>
           <CountDown />
-        </CyclesContext.Provider>
 
         { activeCycle ? (
-          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
             <HandPalm size={24} />
             Interromper
           </StopCountdownButton>
