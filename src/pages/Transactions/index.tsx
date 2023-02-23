e9a3534bb4bab06598591e9052ce6b2a71d88e74
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-02-23 08:52:17 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
-import { TransactionsContainer, TransactionsTable } from "./styles";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
@@ -14,30 +14,24 @@ export function Transactions() {
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
-              <td>R$ 12.000,00</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/02/2023</td>
             </tr>
 
             <tr>
               <td width="50%">Hamburguer</td>
-              <td>- R$ 59,00</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
               <td>Alimentação</td>
               <td>15/02/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Aluguel de apartamento</td>
-              <td>- R$ 1.200,00</td>
-              <td>Casa</td>
-              <td>27/02/2023</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Computador</td>
-              <td>R$ 5.400,00</td>
-              <td>Venda</td>
-              <td>15/03/2023</td>
             </tr>
           </tbody>
         </TransactionsTable>
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -16,6 +16,23 @@ export const TransactionsTable = styled.table`
   td {
     padding: 1.25rem 2rem;
     background: ${props => props.theme['gray-700']};
-  }
 
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome'
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
 `
