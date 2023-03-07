eb531422511b2370f133c5ae20ba32963d1301ac
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-03-07 08:49:46 -0300

    Aplicando context selectors

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import { useContextSelector } from "use-context-selector";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
@@ -8,7 +9,9 @@ import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>
